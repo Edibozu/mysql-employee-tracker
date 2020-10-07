eeaae71613c89b98bde67cbf0c06bdc6ef3b7479
Eduardo Pineda <edibozu@gmail.com>
2020-10-06 23:38:59 -0400

    updated my index.js file to include switch cases and added queries to my seeds.sql file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const { exit } = require("process");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -30,25 +31,38 @@ function init() {
         "Add Employee",
         "Remove Employee",
         "Update Employee Role",
+        "Exit",
       ],
     })
     .then(({ selection }) => {
-      if (selection === "View All Employees") {
-        viewAll();
-      } else if (selection === "View All Employees by Department") {
-        viewEmpByDept();
-      } else if (selection === "Add Employee") {
-        addEmployee();
-      } else if (selection === "Remove Employee") {
-        removeEmployee();
-      } else if (selection === "Update Employee Role") {
-        updateRole();
-      }
-    });
+        switch (selection) {
+            case "View All Employees":
+                return viewAll();
+                break;
+            case "View All Employees by Department":
+                return viewEmpByDept();
+                break;
+            case "Add Employee":
+                return addEmployee();
+                break;
+            case "Remove Employee":
+                return removeEmployee();
+                break;
+            case "Update Employee Role":
+                return updateRole();
+                break;
+            default:
+                console.log("Goodbye");
+                connection.end();
+        }
+    })
 }
 
+// function exit() {
+//   connection.end();
+// }
+
 function viewAll() {
-  console.log("All employees displayed");
   connection.query(
     `SELECT employee.first_name, employee.last_name, role.title, role.salary
         FROM employee
@@ -65,25 +79,71 @@ function viewAll() {
 }
 
 function viewEmpByDept() {
-    console.log("Employees displayed by their department");
-    connection.query("SELECT * FROM department", (err, data) => {
-        departmentArray = data.map(department => department.name)
-        inquirer.prompt([
+  connection.query(`SELECT * FROM department`, (err, data) => {
+    departmentArray = data.map((department) => department.name);
+    inquirer
+      .prompt([
+        {
+          name: "department",
+          type: "list",
+          message: "Which department would you like to view?",
+          choices: departmentArray,
+        },
+      ])
+      .then(({ department }) => {
+        connection.query(
+          `SELECT first_name, last_name, title, salary, name FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id WHEre name = "${department}";`,
+          (err, data) => {
+            if (err) throw err;
+            console.table(data);
+            init();
+          }
+        );
+      });
+  });
+}
+
+function addEmployee() {
+  connection.query(`SELECT * FROM role`, (err, data) => {
+    const rolesArray = data.map((role) => role.title);
+    inquirer
+      .prompt([
+        {
+          name: "firstName",
+          type: "input",
+          message: "Please enter the employee's first name: ",
+        },
+        {
+          name: "lastName",
+          type: "input",
+          message: "Please enter the employee's last name: ",
+        },
+        {
+          name: "role",
+          type: "list",
+          message: "Please enter the employee's role: ",
+          choices: rolesArray,
+        },
+      ])
+      .then(({ firstName, lastName, role }) => {
+        const selectedRole = data.find(
+          (roleObject) => roleObject.title === role
+        );
+        connection.query(
+          `INSERT INTO employee SET ?`,
+          [
             {
-                name: "department",
-                type: "list",
-                message: "Which department would you like to view?",
-                choices: departmentArray
-            }
-        ]).then(({department}) => {
-            connection.query(
-                `SELECT first_name, last_name, title, salary, name FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id WHEre name = "${department}";`,
-                (err, data) => {
-                    if(err) throw err;
-                    console.table(data);
-                    init();
-                }
-            )
-        })
-    })
+              first_name: firstName,
+              last_name: lastName,
+              role_id: selectedRole.id,
+            },
+          ],
+          (err, data) => {
+            if (err) throw err;
+            viewAll();
+            init();
+          }
+        );
+      });
+  });
 }
