ebe433c1e0bce8949b080c10d6331040ecef5f06
Eduardo Pineda <edibozu@gmail.com>
2020-10-06 20:08:56 -0400

    made the initial prompt questions when the application is ran

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
   user: "root",
 
   password: "Edge@062190",
-  database: "employeesDB"
+  database: "employeesDB",
 });
 
 connection.connect((err) => {
@@ -17,3 +17,36 @@ connection.connect((err) => {
   console.log("connected as id " + connection.threadId + "\n");
   init();
 });
+
+function init() {
+  inquirer
+    .prompt({
+      name: "selection",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees by Department",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+      ],
+    })
+    .then(({ selection }) => {
+      if (selection === "View All Employees") {
+        viewAll();
+      } else if (selection === "View All Employees by Department") {
+        viewEmpByDept();
+      } else if (selection === "Add Employee") {
+        addEmployee();
+      } else if (selection === "Remove Employee") {
+        removeEmployee();
+      } else if (selection === "Update Employee Role") {
+        updateRole();
+      }
+    });
+}
+
+function viewAll() {
+
+}
